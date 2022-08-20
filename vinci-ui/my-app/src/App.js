1d62b5cbb542134e35bc7d20c3fe5482a6b7d23b
Omer Gul <omergul2001@gmail.com>
2022-08-20 21:02:39 +1000

    progress

--- vinci-ui/my-app/src/App.js
+++ vinci-ui/my-app/src/App.js
@@ -21,6 +21,7 @@ function App() {
           <header className="wrapper"> </header>
           <div className="content">
               <h1 className='title-text' >Vinci</h1>
+              <p >alpha v0.0</p>
           </div>
       </section>
 
@@ -38,13 +39,17 @@ function App() {
             
           <Grid item xs={12} sm={6} md={3}>
             <div className='rainbow-image-container'> 
-              <img className='display-image' width="500" height="500"  style={{opacity:0}}/>
+              <img className='display-image' width="475" height="475"  style={{opacity:0}}/>
             </div>
+
+            <div className='image-container'> 
+              <img className='display-image' width="475" height="475"  style={{opacity:0}}/>
+            </div>
+            
           </Grid>
 
-          {/* <div className='image-container'> 
-            <img className='display-image' width="500" height="500"/>
-          </div> */}
+       
+            {/* <img className='display-image' width="500" height="500"/> */}
           </Grid>
         <p className='section p'>The vision for vinci is similar to dall-e by Open-Ai, but instead of a text to image converter its a song to image generator. The plan is to have a deep neural network learn the pattern between song and cover art, learning from extensive list of songs and their corresponding art online, from sites like spotify, apple music and so on, generating stunning artwork to match the character and feel of the song </p>
   
